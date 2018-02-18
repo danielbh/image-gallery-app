0bb553915950ece29651b87aa7b687247d566bdc
danielbh <danielhollcraft@gmail.com>
2018-02-18 02:04:13 -0500

    feat: setting up ImageDetails page

--- Scenes/ImageDetails.js
+++ Scenes/ImageDetails.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 const styles = StyleSheet.create({
   container: {
@@ -10,12 +12,44 @@ const styles = StyleSheet.create({
   },
 })
 
-export default class ImageDetails extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Image Details</Text>
-      </View>
-    )
+const ImageDetails = ({
+  user,
+  tags,
+  imageHeight,
+  imageWidth,
+  src
+}) => (
+  <View style={styles.container}>
+    <Image
+      style={{ width: 100, height: 100 }}
+      source={{ uri: src }}
+    />
+    <Text>user: {user}</Text>
+    <Text>tags: {tags}</Text>
+    <Text>resolution: {`${imageWidth}x${imageHeight}`}</Text>
+  </View>
+)
+
+ImageDetails.propTypes = {
+  user: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  imageHeight: PropTypes.number.isRequired,
+  imageWidth: PropTypes.number.isRequired,
+  src: PropTypes.string.isRequired
+}
+
+const mapState = ({ currentImage }) => {
+  const {
+    user, tags, imageHeight, imageWidth, src
+  } = currentImage
+
+  return {
+    user,
+    tags,
+    imageHeight,
+    imageWidth,
+    src
   }
 }
+
+export default connect(mapState)(ImageDetails)
